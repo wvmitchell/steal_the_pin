977816d9ad5a13f418d6e4e391bc96cd62ae3756
Will Mitchell <wvmitchell@gmail.com>
2023-03-27 20:51:25 -0600

    add tests

--- test/index.js
+++ test/index.js
@@ -7,6 +7,19 @@ describe('getPINs', function() {
     assert.isArray(result);
   });
 
+  let expectations = {
+    "8": ["5", "7", "8", "9", "0"],
+    "11": ["11", "22", "44", "12", "21", "14", "41", "24", "42"],
+    "369": ["339","366","399","658","636","258","268","669","668","266","369","398","256","296","259","368","638","396","238","356","659","639","666","359","336","299","338","696","269","358","656","698","699","298","236","239"]
+  };
+
+  for (let pin in expectations) {
+
+    it(`Testing PIN='${pin}'`, () => {
+      assert.sameMembers(getPINs(pin), expectations[pin]);
+    });
+  }
+
   it('should return the observed PIN when it has only one digit', function() {
     const result = getPINs('1');
     assert.lengthOf(result, 3);
